d9b0db56461e4f5aaf09c87c76cd9995e6ac490f
Miloslav Pojman <mpojman@akamai.com>
2020-05-18 19:03:48 +0200

    Quantile function.

--- src/distimate/stats.py
+++ src/distimate/stats.py
@@ -16,16 +16,58 @@ def make_cdf(edges, hist):
     return InterpolatingFunction(edges, y, left=0, right=right)
 
 
+def make_quantile(edges, hist):
+    """Create a quantile function."""
+    edges = np.asarray(edges)
+    hist = np.asarray(hist)
+    cumulative = np.cumsum(hist, dtype=np.float64)
+    if cumulative[-2] == 0:
+        # When we have no samples then the function is undefined.
+        # In addition to that, the function is also undefined
+        # if all samples are in the last bucket (greater than the last edge).
+        x = np.array([0, 1], dtype=np.float64)
+        y = np.array([np.nan, np.nan], dtype=np.float64)
+    else:
+        normed = cumulative[:-1] / cumulative[-1]
+        # Make sure that x values start at zero and end at one.
+        x_candidates = np.r_[0, normed, 1]
+        y_candidates = np.r_[edges[0], edges, np.nan]
+        # If there are vertical chains of points with same x value,
+        # keep only the first and the last point of each chain.
+        # Heading or trailing chains are removed completely.
+        non_zero = np.r_[False, hist != 0, False]
+        mask = non_zero[:-1] | non_zero[1:]
+        x = x_candidates[mask]
+        y = y_candidates[mask]
+    return InterpolatingFunction(x, y, interp=interp_left)
+
+
+def interp_left(v, xp, fp, left=None, right=None):
+    """
+    Like ``np.interp`` but uses lowest of equal xp values.
+
+    >>> np.interp(10, [0, 10, 10, 20], [1, 2, 3, 4])
+    3.0
+    >>> interp_left(10, [0, 10, 10, 20], [1, 2, 3, 4])
+    2.0
+    """
+    v = np.asarray(v)
+    xp = np.asarray(xp)
+    fp = np.asarray(fp)
+    return np.interp(-v, -xp[::-1], fp[::-1], left=right, right=left)
+
+
 class InterpolatingFunction:
     """Function interpolating between sample values."""
 
-    __slots__ = ("x", "y", "left", "right")
+    __slots__ = ("x", "y", "_left", "_right", "_interp")
 
-    def __init__(self, x, y, left=None, right=None):
+    def __init__(self, x, y, *, left=np.nan, right=np.nan, interp=np.interp):
         self.x = x
         self.y = y
-        self.left = left
-        self.right = right
+        self._left = left
+        self._right = right
+        self._interp = interp
 
     def __call__(self, v):
         """
@@ -34,6 +76,4 @@ class InterpolatingFunction:
         :param x: scalar value or Numpy array-like
         :return: scalar value or Numpy array depending on *x*
         """
-        return self._interp(v, self.x, self.y, left=self.left, right=self.right)
-
-    _interp = staticmethod(np.interp)
+        return self._interp(v, self.x, self.y, left=self._left, right=self._right)
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -2,9 +2,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_allclose, assert_array_equal
 
-from distimate.stats import make_cdf
-
-EDGES = [1, 10, 100]
+from distimate.stats import make_cdf, make_quantile
 
 
 def assert_func_values(f, x, y):
@@ -18,7 +16,7 @@ def assert_func_values(f, x, y):
 
 class TestCDF:
     def test_cdf_of_empty(self):
-        cdf = make_cdf(EDGES, [0, 0, 0, 0])
+        cdf = make_cdf([1, 10, 100], [0, 0, 0, 0])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [np.nan, np.nan, np.nan])
         assert_func_values(
@@ -28,7 +26,7 @@ class TestCDF:
         )
 
     def test_cdf_of_first_bin(self):
-        cdf = make_cdf(EDGES, [7, 0, 0, 0])
+        cdf = make_cdf([1, 10, 100], [7, 0, 0, 0])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [1, 1, 1])
         assert_func_values(
@@ -36,7 +34,7 @@ class TestCDF:
         )
 
     def test_cdf_of_last_bin(self):
-        cdf = make_cdf(EDGES, [0, 0, 0, 7])
+        cdf = make_cdf([1, 10, 100], [0, 0, 0, 7])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [0, 0, 0])
         assert_func_values(
@@ -44,7 +42,7 @@ class TestCDF:
         )
 
     def test_cdf_of_inner_bin(self):
-        cdf = make_cdf(EDGES, [0, 7, 0, 0])
+        cdf = make_cdf([1, 10, 100], [0, 7, 0, 0])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [0, 1, 1])
         assert_func_values(
@@ -52,7 +50,7 @@ class TestCDF:
         )
 
     def test_cdf_of_first_and_next_bin(self):
-        cdf = make_cdf(EDGES, [3, 1, 0, 0])
+        cdf = make_cdf([1, 10, 100], [3, 1, 0, 0])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [0.75, 1, 1])
         assert_func_values(
@@ -60,7 +58,7 @@ class TestCDF:
         )
 
     def test_cdf_of_first_and_inner_bin(self):
-        cdf = make_cdf(EDGES, [3, 0, 1, 0])
+        cdf = make_cdf([1, 10, 100], [3, 0, 1, 0])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [0.75, 0.75, 1])
         assert_func_values(
@@ -68,7 +66,7 @@ class TestCDF:
         )
 
     def test_cdf_of_first_and_last_bin(self):
-        cdf = make_cdf(EDGES, [3, 0, 0, 1])
+        cdf = make_cdf([1, 10, 100], [3, 0, 0, 1])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [0.75, 0.75, 0.75])
         assert_func_values(
@@ -78,7 +76,7 @@ class TestCDF:
         )
 
     def test_cdf_of_last_and_inner_bin(self):
-        cdf = make_cdf(EDGES, [0, 3, 0, 1])
+        cdf = make_cdf([1, 10, 100], [0, 3, 0, 1])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [0, 0.75, 0.75])
         assert_func_values(
@@ -88,7 +86,7 @@ class TestCDF:
         )
 
     def test_cdf_of_last_and_prev_bin(self):
-        cdf = make_cdf(EDGES, [0, 0, 3, 1])
+        cdf = make_cdf([1, 10, 100], [0, 0, 3, 1])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [0, 0, 0.75])
         assert_func_values(
@@ -96,10 +94,125 @@ class TestCDF:
         )
 
     def test_cdf_of_inner_bins(self):
-        cdf = make_cdf(EDGES, [0, 3, 1, 0])
+        cdf = make_cdf([1, 10, 100], [0, 3, 1, 0])
         assert_array_equal(cdf.x, [1, 10, 100])
         assert_array_equal(cdf.y, [0, 0.75, 1])
         assert_func_values(
-            cdf, [0.9, 1, 5.5, 10, 55, 100, 101],
-            [0, 0, 3/8, 3/4, 7/8, 1, 1],
+            cdf, [0.9, 1, 5.5, 10, 55, 100, 101], [0, 0, 3 / 8, 3 / 4, 7 / 8, 1, 1],
+        )
+
+    def test_cdf_of_inner_bins_with_gap(self):
+        cdf = make_cdf([1, 10, 100, 1000], [0, 3, 0, 1, 0])
+        assert_array_equal(cdf.x, [1, 10, 100, 1000])
+        assert_array_equal(cdf.y, [0, 0.75, 0.75, 1])
+        assert_func_values(
+            cdf,
+            [0.9, 1, 5.5, 10, 55, 100, 550, 1000, 1001],
+            [0, 0, 3 / 8, 3 / 4, 3 / 4, 3 / 4, 7 / 8, 1, 1],
+        )
+
+
+class TestQuantileFunction:
+    def test_quantile_of_empty(self):
+        quantile = make_quantile([1, 10, 100], [0, 0, 0, 0, 0])
+        assert_array_equal(quantile.x, [0, 1])
+        assert_array_equal(quantile.y, [np.nan, np.nan])
+        assert_func_values(
+            quantile, [-1, 0, 1 / 2, 1, 2], [np.nan, np.nan, np.nan, np.nan, np.nan],
+        )
+
+    def test_quantile_of_first_bin(self):
+        quantile = make_quantile([1, 10, 100], [7, 0, 0, 0])
+        assert_array_equal(quantile.x, [0, 1])
+        assert_array_equal(quantile.y, [1, 1])
+        assert_func_values(
+            quantile, [-1, 0, 1 / 2, 1, 2], [np.nan, 1, 1, 1, np.nan],
+        )
+
+    def test_quantile_of_last_bin(self):
+        quantile = make_quantile([1, 10, 100], [0, 0, 0, 7])
+        assert_array_equal(quantile.x, [0, 1])
+        assert_array_equal(quantile.y, [np.nan, np.nan])
+        assert_func_values(
+            quantile, [-1, 0, 1 / 2, 1, 2], [np.nan, np.nan, np.nan, np.nan, np.nan],
+        )
+
+    def test_quantile_of_inner_bin(self):
+        quantile = make_quantile([1, 10, 100, 1000], [0, 0, 7, 0, 0])
+        assert_array_equal(quantile.x, [0, 1])
+        assert_array_equal(quantile.y, [10, 100])
+        assert_func_values(
+            quantile, [-1, 0, 1 / 2, 1, 2], [np.nan, 10, 55, 100, np.nan],
+        )
+
+    def test_quantile_of_first_inner_bin(self):
+        quantile = make_quantile([1, 10, 100], [0, 7, 0, 0])
+        assert_array_equal(quantile.x, [0, 1])
+        assert_array_equal(quantile.y, [1, 10])
+        assert_func_values(
+            quantile, [-1, 0, 1 / 2, 1, 2], [np.nan, 1, 5.5, 10, np.nan],
+        )
+
+    def test_quantile_of_last_inner_bin(self):
+        quantile = make_quantile([1, 10, 100], [0, 0, 7, 0])
+        assert_array_equal(quantile.x, [0, 1])
+        assert_array_equal(quantile.y, [10, 100])
+        assert_func_values(
+            quantile, [-1, 0, 1 / 2, 1, 2], [np.nan, 10, 55, 100, np.nan],
+        )
+
+    def test_quantile_of_first_and_next_bin(self):
+        quantile = make_quantile([1, 10, 100], [3, 1, 0, 0])
+        assert_array_equal(quantile.x, [0, 0.75, 1])
+        assert_array_equal(quantile.y, [1, 1, 10])
+        assert_func_values(
+            quantile, [0, 3 / 8, 3 / 4, 7 / 8, 1], [1, 1, 1, 5.5, 10],
+        )
+
+    def test_quantile_of_first_and_inner_bin(self):
+        quantile = make_quantile([1, 10, 100], [3, 0, 1, 0])
+        assert_array_equal(quantile.x, [0, 0.75, 0.75, 1])
+        assert_array_equal(quantile.y, [1, 1, 10, 100])
+        assert_func_values(
+            quantile, [0, 3 / 8, 3 / 4, 7 / 8, 1], [1, 1, 1, 55, 100],
+        )
+
+    def test_quantile_of_first_and_last_bin(self):
+        quantile = make_quantile([1, 10, 100], [3, 0, 0, 1])
+        assert_array_equal(quantile.x, [0, 0.75, 0.75, 1])
+        assert_array_equal(quantile.y, [1, 1, 100, np.nan])
+        assert_func_values(
+            quantile, [0, 3 / 8, 3 / 4, 7 / 8, 1], [1, 1, 1, np.nan, np.nan],
+        )
+
+    def test_quantile_of_last_and_inner_bin(self):
+        quantile = make_quantile([1, 10, 100], [0, 3, 0, 1])
+        assert_array_equal(quantile.x, [0, 0.75, 0.75, 1])
+        assert_array_equal(quantile.y, [1, 10, 100, np.nan])
+        assert_func_values(
+            quantile, [0, 3 / 8, 3 / 4, 7 / 8, 1], [1, 5.5, 10, np.nan, np.nan],
+        )
+
+    def test_quantile_of_last_and_prev_bin(self):
+        quantile = make_quantile([1, 10, 100], [0, 0, 3, 1])
+        assert_array_equal(quantile.x, [0, 0.75, 1])
+        assert_array_equal(quantile.y, [10, 100, np.nan])
+        assert_func_values(
+            quantile, [0, 3 / 8, 3 / 4, 7 / 8, 1], [10, 55, 100, np.nan, np.nan],
+        )
+
+    def test_quantile_of_inner_bins(self):
+        quantile = make_quantile([1, 10, 100], [0, 3, 1, 0])
+        assert_array_equal(quantile.x, [0, 0.75, 1])
+        assert_array_equal(quantile.y, [1, 10, 100])
+        assert_func_values(
+            quantile, [0, 3 / 8, 3 / 4, 7 / 8, 1], [1, 5.5, 10, 55, 100],
+        )
+
+    def test_quantile_of_inner_bins_with_gap(self):
+        quantile = make_quantile([1, 10, 100, 1000], [0, 3, 0, 1, 0])
+        assert_array_equal(quantile.x, [0, 0.75, 0.75, 1])
+        assert_array_equal(quantile.y, [1, 10, 100, 1000])
+        assert_func_values(
+            quantile, [0, 3 / 8, 3 / 4, 7 / 8, 1], [1, 5.5, 10, 550, 1000],
         )
